b05ca584a7fd1234740259b3ccdef1919948d671
kuku8990 <kuku8990@gmail.com>
2019-01-12 13:31:32 +0800

    Remove android links

--- android/app/src/main/java/com/testandroidgeolocationservices/MainApplication.java
+++ android/app/src/main/java/com/testandroidgeolocationservices/MainApplication.java
@@ -7,8 +7,6 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.gavin.RNAndroidGeolocationService.RNAndroidGeolocationServicePackage;
-
 import java.util.Arrays;
 import java.util.List;
 
@@ -23,8 +21,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new RNAndroidGeolocationServicePackage()
+          new MainReactPackage()
       );
     }
 
